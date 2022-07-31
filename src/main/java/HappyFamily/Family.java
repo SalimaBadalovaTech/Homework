92fa770c912ca2e206350d1c7fa1d78855a10d13
salimabadalovatech <sbedelova@std.beu.edu.az>
2022-07-31 13:05:43 +0400

    The Happy Family project

--- src/main/java/HappyFamily/Family.java
+++ src/main/java/HappyFamily/Family.java
@@ -1,5 +1,8 @@
 package HappyFamily;
 
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
 public class Family {
 
   Human mother;
@@ -14,7 +17,41 @@ public class Family {
     this.pet = pet;
   }
 
+public static void to_welcome_the_favorite (String name) {
 
+  System.out.println("Welcome, " + name);
+}
+
+  public static void describe_the_favorite (Human human) {
+
+    System.out.println(human.toString());
+  }
+
+  public static void feed (Human human) {
+    Human a = new Human();
+
+    System.out.println("I am feeding " + a.getName());
+  }
+
+  public static void addChild (Human human, int ith_Child, int numberOfChildren) {
+
+    Human a = new Human();
+
+    Human[] children = new Human[numberOfChildren];
+    children[ith_Child] = a;
+
+  }
+
+  public static boolean deleteChildren (int ith_Child, Human[] children) {
+
+      return true;
+  }
+
+public static int countFamily (Human[] children) {
+
+    int count = 2  + children.length;
+    return count;
+}
 
 
   }
